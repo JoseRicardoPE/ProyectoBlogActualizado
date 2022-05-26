698578383bd01d34feb687926e6ee899ddf7554c
facundouy <97556086+facundouy@users.noreply.github.com>
2022-05-26 13:39:20 -0300

    JWT no funcionando

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const apiRouter = express.Router();
 const apiController = require("../controllers/apiController");
+const checkJwt = require("express-jwt");
+const authConfig = require("../auth");
 
 // Rutas API's
-apiRouter.get("/", apiController.showArticle);
+apiRouter.get(
+  "/prueba/:id",
+  checkJwt({ secret: authConfig.secret, algorithms: ["HS256"] }),
+  async (req, res) => {
+    const articleId = req.params.id;
+    const articleIndividual = await Article.findByPk(articleId, {
+      include: [Author, Comment],
+    });
+    res.json({ articleIndividual });
+  }
+);
 
 module.exports = apiRouter;
